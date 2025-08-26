94091118e10ae35cc5cf4c7f046c80dee5e710f1
Pedro Santos <pedrosantos.tds2.senai@gmail.com>
2025-08-26 11:17:11 -0300

    feat: create throw error test

--- __tests__/calculadora.test.js
+++ __tests__/calculadora.test.js
@@ -1,4 +1,4 @@
-import { somar, subtrair } from "../src/calculadora.js";
+import { somar, subtrair, multiplicar, dividir } from "../src/calculadora.js";
 
 describe("Calculadora", () => {
   test("deve somar dois números corretamente", () => {
@@ -27,4 +27,33 @@ describe("Calculadora", () => {
     //Assert(verificar)
     expect(resultado).toBe(10);
   });
+  test("deve multiplicar dois números corretamente", () => {
+    //AAA
+
+    //Arrange(preparar)
+    const a = 4;
+    const b = 3;
+
+    //Act(executar)
+    const resultado = multiplicar(a, b);
+
+    //Assert(verificar)
+    expect(resultado).toBe(12);
+  }); 
+   test("deve dividir dois números corretamente", () => {
+      //AAA
+   
+      //Arrange(preparar)
+      const a = 10;
+      const b = 2;
+   
+      //Act(executar)
+      const resultado = dividir(a, b);
+   
+      //Assert(verificar)
+      expect(resultado).toBe(5);
+   });
+   test("deve retornar erro ao dividir por zero", () => {
+      expect(() => dividir(10, 0)).toThrow("Divisão por zero não é permitida.");
+   });
 });
